e903b150102e5c09b8c6dba5f24e12a4b775cd0b
Alibongo <oioimolloy@gmail.com>
2017-01-18 10:12:46 +0000

    changes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //db name etc
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONG 'mongodb://localhost/cycle-app',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/cycle-app',
   secret: process.env.SECRET || 'Secret hush hush'
 };
